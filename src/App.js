9e44edbc2025b09247e95c03ad0488628201d205
Rugved Taru <tarurugved007@gmail.com>
2024-07-04 22:48:23 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
       <h1 style={{ textAlign: "center" }} className="text-6xl">
-        Hello All
+        Hello All this is updated website address routing
       </h1>
 
       <p className="text-4xl font-bold font-signature">
