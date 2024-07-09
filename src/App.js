cb27420a0eaa8cd8fc591ce4e5e7c5755e8b37e5
Rugved Taru <tarurugved007@gmail.com>
2024-07-09 15:21:04 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
+import NavBar from "./components/NavBar";
+
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <h1 style={{ textAlign: "center" }} className="text-6xl">
-        Hello All this is updated website address routing
-      </h1>
-
-      <p className="text-4xl font-bold font-signature">
-       Good
-      </p>
+      <NavBar/>
     </div>
   );
 }
