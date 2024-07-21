d8bd33a9da61dd6acf96c35442574ce65b583c4a
Rugved Taru <tarurugved007@gmail.com>
2024-07-21 20:55:54 +0530

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,15 +2,40 @@ import React from 'react';
 import {FaBars,FaTimes  } from "react-icons/fa";
 
 const NavBar = () => {
+  const links=[
+    {
+      id:1,
+      link: 'home'
+    },
+    {
+      id:2,
+      link: 'about'
+    },
+    {
+      id:3,
+      link: 'portfolio'
+    },
+    {
+      id:4,
+      link: 'experience'
+    },
+    {
+      id:5,
+      link: 'contact'
+    },
+  ]
   return (
     <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
         <div>
           <h1 className='text-5xl font-signature ml-2'>Rugved</h1>  
         </div>
     <ul className='flex'>
-        <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-150'>Home</li>
-        <li className=''>Home</li>
-        <li className=''>Home</li>
+      {
+        links.map(link=>(
+          <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-150'>Home</li>
+        ))
+      }
+      
 
     </ul>
     </div>
