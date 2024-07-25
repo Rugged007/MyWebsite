1dcfea74d3baa4998ac3398d4fce5dfea03850ef
Rugved Taru <tarurugved007@gmail.com>
2024-07-25 23:30:20 +0530

    Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,12 +27,12 @@ const NavBar = () => {
   return (
     <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
         <div>
-          <h1 className='text-5xl font-signature ml-2'>Rugved</h1>  
+          <h3 className='text-5xl font-signature ml-2'>Rugved</h3>  
         </div>
     <ul className='flex'>
       {
-        links.map(link=>(
-          <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-150'>Home</li>
+        links.map(({id,link})=>(
+          <li key={link.id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-150'>{link}</li>
         ))
       }
       
