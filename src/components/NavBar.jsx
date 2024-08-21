8b4897b22e1ecda3174ee8262f9cc342a02b1ff4
Rugved Taru <tarurugved007@gmail.com>
2024-08-22 00:18:27 +0530

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -46,12 +46,18 @@ const NavBar = () => {
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
+      {nav &&(
       <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500"> 
-        <li className="">home</li>
-        <li>hone</li>
-        <li>hone</li>
-    
+      {links.map(({ id, link }) => (
+          <li
+            key={link.id}
+            className="px-4 cursor-pointer capitalize py-6 text-4xl"
+          >    
+            {link}
+          </li>
+        ))} 
       </ul>
+      )}
     </div>
   );
 };
