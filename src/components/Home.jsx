1bd76565e92b0c3520524ffeb0eabb20603a1f4b
Rugved <tarurugved007@gmail.com>
2024-09-29 20:40:08 +0530

    Links

---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import heroImage from "../assets/portfolio/heroImage.jpg";
+import { IoMdArrowRoundForward } from "react-icons/io";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 text-white"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Full Stack Developer</h2>
+          <p className="text-gray-500 py-4 max-w-full">
+            Motivated and dedicated MCA student in the last year at MIT College,
+            Pune, with a strong passion for computer science and technology.
+            Possessing a solid foundation in programming languages, algorithms,
+            and database management. Currently seeking opportunities to gain
+            hands-on experience in a dynamic professional environment.
+          </p>
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+              Porfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src={heroImage}
+            alt="My Profile"
+            className="rounded-2xl mx-auto w-2/3"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
