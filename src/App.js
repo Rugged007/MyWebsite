63b30c653cb4ccee6255c4cac3b410b9c0a20c01
Rugved <tarurugved007@gmail.com>
2024-10-02 17:42:59 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Portfoili from "./components/Portfoili";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 
 export default function App() {
   return (
-    <div>
+    <div> 
       <NavBar />
       <Home />
-      {/* <About /> */}
+      <About /> 
+      <Portfoili />
       <SocialLinks />
     </div>
   );
